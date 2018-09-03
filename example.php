4536e637a5c786a44b51d82fdedba666d2647014
Anang Hanafi <ananghanafi13@gmail.com>
2018-09-03 15:49:17 +0700

    ss

---
+++ example.php
@@ -0,0 +1,61 @@
+<?php
+
+
+/*$file = fopen('DatasetIbuHamil.csv', 'r');
+while (!feof($file)) {//membaca file dari awal sampai akhir
+	$content = fgetcsv($file); //membaca file
+	$count = count($content);
+	for ($i=0; $i <$count ; $i++) { 
+		echo $content[$i]."\n";
+	}
+	echo "<br/>";
+}
+while (($line = fgetcsv($file)) !== FALSE) {
+  //$line is an array of the csv elements
+  print_r($line);
+}
+fclose($file);*/
+
+$file = fopen("DatasetIbuHamil.csv","r");
+
+while(! feof($file))
+  {
+ print_r(fgetcsv($file));
+ // 	echo Array[9];
+  }
+
+fclose($file);
+
+// $csvFile ='DatasetIbuHamil.csv';
+// function readCSV($csvFile){
+//     $file_handle = fopen('DatasetIbuHamil.csv', 'r');
+//     while (!feof($file_handle) ) {
+//         $line_of_text[] = fgetcsv($file_handle, 10000);
+//     }
+//     fclose($file_handle);
+//     return $line_of_text;
+// }
+ 
+ 
+// // // Set path to CSV file
+// // // $csvFile = 'test.csv';
+// $csv = readCSV($csvFile);
+// echo '<pre>';
+// print_r($csv);
+// echo '</pre>';
+
+// $namafile = $_FILES['filecsv']['tmp_name']
+// $row = 1;
+// if (($handle = fopen('DatasetIbuHamil.csv', "r")) !== FALSE) {
+//     while (($data = fgetcsv($handle, 10000, ",","'")) !== FALSE) {
+//         $num = count($data);
+//         echo "<p> $num data di baris $row: <br /></p>\n";
+//         $row++;
+//         for ($c=0; $c < $num; $c++) {
+//             echo $data[$c] ;
+//         }
+//     }
+//     fclose($handle);
+// }
+
+?>
---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Algoritma K-Medoid</title>
+
+	<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	<link rel="shortcut icon" type="image/x-icon" href="images/icon.jpg">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+  <link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+</head>
+<body >
+    <div class="preloader">
+    <div class="loader theme_background_color">
+        <span></span>
+    </div>
+</div>
+
+<div class="wrapper">
+    <nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <!-- Responsive Buat Mobile -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <a class="navbar-brand" href="index.php">Algoritma <span class="themecolor"> K-Medoid</span></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#home" class="page-scroll">HOME</a></li>
+						<li><a href="#one" class="page-scroll">ABOUT</a></li>
+            <li><a href="#two" class="page-scroll">CALCULATE</a></li>
+<!--             <li><a href="#three" class="page-scroll">TEAM</a></li> -->
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+   <section class="main-heading" id="home">
+       <div class="overlay">
+           <div class="container">
+               <div class="row">
+                   <div class="main-heading-content col-md-12 col-sm-12 text-center">
+        		 					<h1 class="main-heading-title"><span class="main-element themecolor" data-elements="WELCOME, MENGGUNAKAN K-MEDOIDS"></span></h1>
+        							<p class="main-heading-text" style="font-size:18px;">Pada kesempatan kali ini kita akan menjelaskan penerapan Algoritma K-Medoids<br/>Tentunya masih ingat kan Algoritma K-Medoids itu apa. Jika sudah silahkan pilih hitung jika belum pilih penjelasan.</p>
+        							<div class="btn-bar">
+							          <a href="hitung.php" class="btn btn-custom theme_background_color" id="btn-1">Hitung</a>
+							          <a href="#one" class="btn btn-custom-outline page-scroll" id="btn-2">Penjelasan</a>
+        							</div>
+      						</div>
+               </div>
+           </div>
+       </div>
+   </section>
+
+
+ <!-- [ABOUT US]
+ ============================================================================================================================-->
+ <section class="aboutus white-background black" id="one">
+     <div class="container" >
+         <div class="row" >
+             <div class="col-md-12 text-center black">
+                 <h2 class="title">Apa itu <span class="themecolor">Algoritma K-Medoids ?</span></h2>
+            			<p class="a-slog">
+										<b>"</b><br/>
+										<b>Algoritma K-Medoids</b> adalah salah satu algoritma yang digunakan untuk klasifikasi atau pengelompokan data.
+									<br/><br/>
+										<b>Prediksi</b> adalah suatu cara Untuk mencari kemungkinan hasil dari suatu hal pada masa yang akan dating.
+										Pada teknologi data mining, metode peramalan digunakan sebagai alat bantu memprediksi kemungkinan yang akan dating berdasarkan bukti-bukti temuan pada data.
+										Sedangkan variable adalah besaran yang berubah-ubah nilainya.
+										<br/><b>"<b>
+									</p>
+             </div>
+         </div>
+
+         <div class="gap">
+
+         </div>
+
+     </div>
+ </section>
+
+
+ <!-- [RECENT-WORKS
+ ============================================================================================================================-->
+     <section class="recent-works text-center" id="two" style="background-color: #1c1c1c;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+              <h2 class="title"Sudah Paham Mengenai <span class="themecolor">Algoritma K-Medoids</span></h3>
+            <p>Jika sudah paham mengenai <b>Algoritma K-Medoids</b>, Mari kita hitung data mining menggunakan <b>Algoritma K-Medoids</b></p>
+          </div>
+        </div>
+
+          <div class="gap"></div>
+
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <a href="hitung.php" class="btn btn-custom theme_background_color" id="btn-1">Hitung</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+  <!-- [OUR TEAM]
+ ============================================================================================================================-->
+<!--   <section class="our-team white-background black" id="three">
+      <div class="container">
+       <div class="row text-center">
+          <div class="col-md-12" id="team">
+              <h3 class="title">Team<span class="themecolor"> Kita</span></h3>
+
+          </div>
+        </div>
+      </div>
+  </section>
+ -->
+ <!-- [/OUR TEAM]
+ ============================================================================================================================-->
+
+<!-- <section class="sub-form text-center" id="eight">
+  <div class="container">
+    <div class="col-md-12">
+        <h2 class="title">TERIMA<span class="themecolor"> KASIH</span></h2>
+         <!--    <p class="lead">Semoga ilmu yang di berikan bisa bermanfaat yaa</p> -->
+    </div>
+
+  </div></section>  
+
+
+<footer class="site-footer section-spacing text-center " id="eight">
+
+  <div class="container">
+    <div class="row">
+			<div class="col-md-2"> <small id="memberteam">Saya</small></div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</div>
+
+	<script src="library/modernizr.custom.97074.js"></script>
+	<script src="library/jquery-1.11.3.min.js"></script>
+  <script src="library/bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script src="library/vegas/vegas.min.js"></script>
+	<script src="js/plugins.js"></script>
+  <script src="js/typed.js"></script>
+  <script src="js/fappear.js"></script>
+  <script src="js/jquery.countTo.js"></script>
+	<script src="js/owl.carousel.js"></script>
+  <script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="js/SmoothScroll.js"></script>
+
+	<script src="js/common.js"></script>
+
+</body>
+</html>
